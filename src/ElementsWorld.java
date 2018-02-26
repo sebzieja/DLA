131aff140596e5fd3b4bcb610aa2127a264dc6ef
Sebastian Zieja <yowikun@gmail.com>
2018-02-26 09:06:05 +0100

    Sticking function

--- src/ElementsWorld.java
+++ src/ElementsWorld.java
@@ -1,19 +1,17 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Random;
-import java.util.ArrayList;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 import javax.swing.*;
 
 public class ElementsWorld extends JPanel {
     private static final int UPDATE_RATE = 30;  // Frames per second (fps)
 
-    private Rectangle rectangle;         // A single bouncing Rectangle's instance
     private ContainerBox box;  // The container rectangular box
-
     private DrawCanvas canvas; // Custom canvas for drawing the box/rectangle
     private int canvasWidth;
     private int canvasHeight;
-    private static int howManyRectangles = 50;
+    private static int howManyRectangles = 100;
     //    private static Rectangle[] rectangles = new Rectangle[howManyRectangles];
     private static ArrayList<Rectangle> rectangles = new ArrayList<>();
 
@@ -25,16 +23,17 @@ public class ElementsWorld extends JPanel {
         canvasHeight = height;
 
         // Init the rectangle at a random location (inside the box) and moveAngle
-        Random rand = new Random();
-        int widthRectangle = 20;
-        int heightRectangle = 20;
-        int x = 0;
-        int y = 0;
+        int widthRectangle = 10;
+        int heightRectangle = 10;
 
-        int speed = 20;
+        int speed = 5;
         for (int i = 0; i < howManyRectangles; i++) {
-            rectangles.add(new Rectangle(widthRectangle, heightRectangle, canvasWidth, canvasHeight, speed, Color.BLUE));
+            rectangles.add(createRectangle(widthRectangle, heightRectangle, canvasWidth, canvasHeight, 1, Color.BLUE));
         }
+        Rectangle stopped = new Rectangle((canvasWidth - 20) / 2, (canvasHeight - 20) / 2, 20, 20, 0, 15, Color.RED);
+        stopped.stopped = true;
+        rectangles.add(stopped);
+
 
         // Init the Container Box to fill the screen
         box = new ContainerBox(0, 0, canvasWidth, canvasHeight, Color.BLACK, Color.WHITE);
@@ -61,36 +60,35 @@ public class ElementsWorld extends JPanel {
         gameStart();
     }
 
+    public Rectangle createRectangle(int widthRectangle, int heightRectangle, int canvasWidth, int canvasHeight, int speed, Color color) {
+        return new Rectangle(widthRectangle, heightRectangle, canvasWidth, canvasHeight, speed, color);
+    }
+
 
     public void gameStart() {
         // Run the game logic in its own thread.
         Thread gameThread = new Thread(() -> {
             while (true) {
-                long beginTimeMillis, timeTakenMillis, timeLeftMillis;
-                beginTimeMillis = System.currentTimeMillis();
                 // Execute one time-step for the game
                 gameUpdate();
                 // Refresh the display
                 repaint();
                 // Provide the necessary delay to meet the target rate
-                timeTakenMillis = System.currentTimeMillis() - beginTimeMillis;
-                timeLeftMillis = 1000L / UPDATE_RATE - timeTakenMillis;
-                if (timeLeftMillis < 5) timeLeftMillis = 5; // Set a minimum
                 // Delay and give other thread a chance
-                try {
-                    Thread.sleep(timeLeftMillis);
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
-                }
+//                try {
+//                    Thread.sleep(1);
+//                } catch (InterruptedException ex) {
+//                    ex.printStackTrace();
+//                }
             }
         });
-        gameThread.start();  // Invoke GaemThread.run()
+        gameThread.start();  // Invoke GameThread.run()
     }
 
 
     public void gameUpdate() {
         for (Rectangle rectangle : rectangles) {
-            rectangle.moveOneStepCollision(box);
+            moveOneStepCollision(box, rectangle);
         }
 
     }
@@ -114,6 +112,42 @@ public class ElementsWorld extends JPanel {
         @Override
         public Dimension getPreferredSize() {
             return (new Dimension(canvasWidth, canvasHeight));
+        }
+    }
+
+    public void moveOneStepCollision(ContainerBox box, Rectangle rectangle) {
+        // Get the rectangle's bounds, offset by the radius of the rectangle
+
+
+        float rectangleMinX = box.minX + 1;
+        float rectangleMinY = box.minY + 1;
+        float rectangleMaxX = box.maxX - rectangle.height - 1;
+        float rectangleMaxY = box.maxY - rectangle.width - 1;
+
+        // Calculate the rectangle's new position
+        rectangle.x += (ThreadLocalRandom.current().nextFloat() - 0.5) * rectangle.speed;
+        rectangle.y += (ThreadLocalRandom.current().nextFloat() - 0.5) * rectangle.speed;
+        for (Rectangle rect : rectangles) {
+            if (!rectangle.stopped && rect.stopped && (rect != rectangle)) {
+                if (rectangle.intersect(rect)) {
+                    rectangle.speed = 0;
+                    rectangle.color = Color.RED;
+                    rectangle.stopped = true;
+                }
+            }
+        }
+
+        // Check if the rectangle moves over the bounds. If so, adjust the position and speed.
+        if (rectangle.x < rectangleMinX) {
+            rectangle.x = rectangleMinX;     // Re-position the rectangle at the edge
+        } else if (rectangle.x > rectangleMaxX) {
+            rectangle.x = rectangleMaxX;
+        }
+        // May cross both x and y bounds
+        if (rectangle.y < rectangleMinY) {
+            rectangle.y = rectangleMinY;
+        } else if (rectangle.y > rectangleMaxY) {
+            rectangle.y = rectangleMaxY;
         }
     }
 }
--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,8 +2,14 @@ import java.awt.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Rectangle {
-    private float x, y, x2, y2, width, height, speed, speedX, speedY;
-    private Color color;
+    public float x, y, x2, y2, width, height, speed, speedX, speedY;
+    public Color color;
+    public boolean stopped;
+
+    {
+        stopped = false;
+    }
+
     private static final Color DEFAULT_COLOT = Color.RED;
 
     /*
@@ -27,53 +33,55 @@ public class Rectangle {
 
     /*Constructor with random placement on the borders*/
     public Rectangle(float width, float height, float canvasWidth, float canvasHeight, float speed, Color color) {
-        float p = (float) (ThreadLocalRandom.current().nextDouble(0, canvasWidth * 2 + canvasHeight * 2));
-        if (p < (canvasHeight + canvasWidth)) {
-            if (p < canvasWidth) {
-                this.x = p;
-                this.y = 0;
-            } else {
-                this.x = canvasWidth;
-                this.y = p - canvasWidth;
-            }
-
-        } else {
-            p -= (canvasWidth + canvasHeight);
-            if (p < canvasWidth) {
-                this.x = canvasWidth - p;
-                this.y = canvasHeight;
-            } else {
-                this.x = 0;
-                this.y = canvasHeight - (p - canvasWidth);
-            }
-        }
+//        float p = (float) (ThreadLocalRandom.current().nextDouble(0, canvasWidth * 2 + canvasHeight * 2));
+        this.x = (float) (ThreadLocalRandom.current().nextDouble(0, canvasWidth));
+        this.y = (float) (ThreadLocalRandom.current().nextDouble(0, canvasHeight));
         this.width = width;
         this.height = height;
         this.color = color;
         this.speed = speed;
-        this.speedX = (float) (speed * Math.cos(Math.toRadians((15))));
-        this.speedY = (float) (-speed * Math.sin(Math.toRadians((15))));
+        this.speedX = 0;
+        this.speedY = 0;
 
     }
+//    public Rectangle(float width, float height, float canvasWidth, float canvasHeight, float speed, Color color) {
+//        float p = (float) (ThreadLocalRandom.current().nextDouble(0, canvasWidth * 2 + canvasHeight * 2));
+//        if (p < (canvasHeight + canvasWidth)) {
+//            if (p < canvasWidth) {
+//                this.x = p;
+//                this.y = 0;
+//            } else {
+//                this.x = canvasWidth;
+//                this.y = p - canvasWidth;
+//            }
+//
+//        } else {
+//            p -= (canvasWidth + canvasHeight);
+//            if (p < canvasWidth) {
+//                this.x = canvasWidth - p;
+//                this.y = canvasHeight;
+//                this.x2 = canvasWidth - p + width;
+//                this.y2 = canvasWidth + height;
+//            } else {
+//                this.x = 0;
+//                this.y = canvasHeight - (p - canvasWidth);
+//            }
+//        }
+//        this.width = width;
+//        this.height = height;
+//        this.color = color;
+//        this.speed = speed;
+//        this.speedX = (float) (speed * Math.cos(Math.toRadians((15))));
+//        this.speedY = (float) (-speed * Math.sin(Math.toRadians((15))));
+//
+//    }
 
-    public float getX() {
-        return x;
-    }
 
-    public float getY() {
-        return y;
-    }
-
-    public float getX2() {
-        return x2;
-    }
-
-    public float getY2() {
-        return y2;
-    }
-
-    public boolean intersect(Rectangle rectangle2) {
-        return this.getX() < rectangle2.getX2() && this.getX2() > rectangle2.getX() && this.getY() > rectangle2.getY2() && this.getY2() < rectangle2.getY();
+    public boolean intersect(Rectangle rectangle) {
+        if (this.x < (rectangle.x + rectangle.width) && (this.x + this.width) > rectangle.x && this.y < (rectangle.y + rectangle.height) && (this.y + this.height) > rectangle.y) {
+            return true;
+        }
+        return false;
     }
 
     public void draw(Graphics graphics) {
@@ -84,33 +92,6 @@ public class Rectangle {
 
     //TODO Collision Detection
     /*Move one move, check for collision and react if collision occurs*/
-    public void moveOneStepCollision(ContainerBox box) {
-        // Get the rectangle's bounds, offset by the radius of the rectangle
-        float rectangleMinX = box.minX + 1;
-        float rectangleMinY = box.minY + 1;
-        float rectangleMaxX = box.maxX - height - 1;
-        float rectangleMaxY = box.maxY - width - 1;
 
-        // Calculate the rectangle's new position
-        x += (ThreadLocalRandom.current().nextFloat() - 0.5) * speed;
-        y += (ThreadLocalRandom.current().nextFloat() - 0.5) * speed;
-
-        // Check if the rectangle moves over the bounds. If so, adjust the position and speed.
-        if (x < rectangleMinX) {
-            speedX = -speedX; // Reflect along normal
-            x = rectangleMinX;     // Re-position the rectangle at the edge
-        } else if (x > rectangleMaxX) {
-            speedX = -speedX;
-            x = rectangleMaxX;
-        }
-        // May cross both x and y bounds
-        if (y < rectangleMinY) {
-            speedY = -speedY;
-            y = rectangleMinY;
-        } else if (y > rectangleMaxY) {
-            speedY = -speedY;
-            y = rectangleMaxY;
-        }
-    }
 }
 
